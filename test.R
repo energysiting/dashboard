bdc66ca87f013338153edd1e2f2e6634b5f88f2e
danielkerstan <danielkerstan@bren.ucsb.edu>
2022-05-11 22:15:20 +0000

    added wind analysis plots

---
+++ test.R
@@ -0,0 +1,15 @@
+library(terra)
+library(mapview)
+library(raster)
+
+
+test <- rast("/capstone/energysiting/data/analysis/randomForest_Pred_wind_2017.tif")
+
+terra::plot(test)
+
+plot(test)
+
+
+raster::plot(test)
+
+
